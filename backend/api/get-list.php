f6ab53fd011a44cf3eb6aabdbaccc2b224126e4c
davidemarchini <marchini73@gmail.com>
2024-02-29 23:34:48 +0100

    api

--- backend/api/get-list.php
+++ backend/api/get-list.php
@@ -1,10 +1,11 @@
 <?php 
 
 
-echo "Qui verrà stampata la lista";
-
+// leggiamo il contenuto del file json sotto forma di stringa
 $json_list_content = file_get_contents('../data/todolist.json');
 
-header('Content-type: application/json');
+// avvisiamo il browser che riceverà del json
+header('Content-Type: application/json');
 
+// stampiamo il contenuto
 echo $json_list_content;
